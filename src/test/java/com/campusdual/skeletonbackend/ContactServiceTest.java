804dbbf8abb6a0d332476711aee0796affb6c7f1
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-03-22 08:45:24 +0100

    Modify application.yml to fix database naming Add example tests

--- src/test/java/com/campusdual/skeletonbackend/ContactServiceTest.java
+++ src/test/java/com/campusdual/skeletonbackend/ContactServiceTest.java
@@ -15,122 +15,126 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class ContactServiceTest {
 
-	@Mock
-	ContactDao contactDao;
+    @Mock
+    ContactDao contactDao;
 
-	@InjectMocks
-	ContactService contactService;
+    @InjectMocks
+    ContactService contactService;
 
-	@Test
-	void getAllContactsTest() {
-		List<Contact> contactsList = new ArrayList<Contact>();
+    @Test
+    void getAllContactsTest() {
+        List<Contact> contactsList = new ArrayList<Contact>();
 
 
-		Contact contactOne = new Contact("One");
-		Contact contactTwo = new Contact("Two");
-		Contact contactThree = new Contact("Three");
+        Contact contactOne = new Contact("One");
+        Contact contactTwo = new Contact("Two");
+        Contact contactThree = new Contact("Three");
 
-		contactsList.add(contactOne);
-		contactsList.add(contactTwo);
-		contactsList.add(contactThree);
+        contactsList.add(contactOne);
+        contactsList.add(contactTwo);
+        contactsList.add(contactThree);
 
-		when(this.contactDao.findAll()).thenReturn(contactsList);
+        when(this.contactDao.findAll()).thenReturn(contactsList);
 
-		List<ContactDTO> empList = contactService.queryAllContact();
+        List<ContactDTO> empList = this.contactService.queryAllContact();
 
-		verify(contactDao, times(1)).findAll();
-		assertEquals(3, empList.size());
-	}
+        verify(this.contactDao, times(1)).findAll();
+        assertEquals(3, empList.size());
+    }
 
-	@Test
-	void getOneContactTest() {
-		Contact contact = new Contact();
-		contact.setId(1);
+    @Test
+    void getOneContactTest() {
+        Contact contact = new Contact();
+        contact.setId(1);
 
-		when(this.contactDao.getReferenceById(1)).thenReturn(contact);
+        when(this.contactDao.getReferenceById(1)).thenReturn(contact);
 
-		ContactDTO contactDTO = new ContactDTO();
-		contactDTO.setId(1);
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setId(1);
 
-		ContactDTO contactResult = contactService.queryContact(contactDTO);
+        ContactDTO contactResult = this.contactService.queryContact(contactDTO);
 
-		assertNotNull(contactResult);
-	}
+        assertNotNull(contactResult);
+    }
 
-	@Test
-	void contactNotPresentInDb() {
-		when(this.contactDao.getReferenceById(1)).thenReturn(null);
+    @Test
+    void contactNotPresentInDb() {
+        when(this.contactDao.getReferenceById(1)).thenReturn(null);
 
-		ContactDTO contactDTO = new ContactDTO();
-		contactDTO.setId(1);
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setId(1);
 
-		assertNull(contactService.queryContact(contactDTO));
+        assertNull(this.contactService.queryContact(contactDTO));
 
-	}
+    }
 
-	@Test
-	void addContactTest() {
-		Contact contact = new Contact();
-		contact.setId(1);
-		contact.setName("One");
-		contact.setSurname1("Surname1One");
-		contact.setSurname2("Surname2One");
-		contact.setPhone("666555444");
-		contact.setEmail("contact-one@gmail.com");
+    @Test
+    void addContactTest() {
+        Contact contact = new Contact();
+        contact.setId(1);
+        contact.setName("One");
+        contact.setSurname1("Surname1One");
+        contact.setSurname2("Surname2One");
+        contact.setPhone("666555444");
+        contact.setEmail("contact-one@gmail.com");
 
-		ContactDTO createContactDTO = ContactMapper.INSTANCE.toDTO(contact);
+        ContactDTO createContactDTO = ContactMapper.INSTANCE.toDTO(contact);
 
-		when(this.contactDao.saveAndFlush(any(Contact.class))).thenReturn(contact);
-		int contactInsertedId = contactService.insertContact(createContactDTO);
+        when(this.contactDao.saveAndFlush(any(Contact.class))).thenReturn(contact);
+        Integer contactInsertedId = this.contactService.insertContact(createContactDTO);
 
-		verify(contactDao, times(0)).findAll();
-		verify(contactDao, times(1)).saveAndFlush(any(Contact.class));
-		assertNotNull(contactInsertedId);
-		assertEquals(1, contactInsertedId);
-	}
+        verify(this.contactDao, times(0)).findAll();
+        verify(this.contactDao, times(1)).saveAndFlush(any(Contact.class));
+        assertNotNull(contactInsertedId);
+        assertEquals(1, contactInsertedId);
+    }
 
-	@Test
-	void editContactTest() {
-		Contact contact = new Contact();
-		contact.setId(1);
-		contact.setName("One");
-		contact.setSurname1("Surname1One");
-		contact.setSurname2("Surname2One");
-		contact.setPhone("666555444");
-		contact.setEmail("contact-one@gmail.com");
+    @Test
+    void editContactTest() {
+        Contact contact = new Contact();
+        contact.setId(1);
+        contact.setName("One");
+        contact.setSurname1("Surname1One");
+        contact.setSurname2("Surname2One");
+        contact.setPhone("666555444");
+        contact.setEmail("contact-one@gmail.com");
 
-		ContactDTO editContactDTO = ContactMapper.INSTANCE.toDTO(contact);
+        ContactDTO editContactDTO = ContactMapper.INSTANCE.toDTO(contact);
 
-		when(this.contactDao.saveAndFlush(any(Contact.class))).thenReturn(contact);
+        when(this.contactDao.saveAndFlush(any(Contact.class))).thenReturn(contact);
 
-		int editContactId = contactService.updateContact(editContactDTO);
+        Integer editContactId = this.contactService.updateContact(editContactDTO);
 
-		assertNotNull(editContactId);
-		assertEquals(1, editContactId);
-	}
+        assertNotNull(editContactId);
+        assertEquals(1, editContactId);
+    }
 
-	@Test
-	void deleteContactTest() {
-		Contact contact = new Contact();
-		contact.setId(1);
-		contact.setName("One");
-		contact.setSurname1("Surname1One");
-		contact.setSurname2("Surname2One");
-		contact.setPhone("666555444");
-		contact.setEmail("contact-one@gmail.com");
+    @Test
+    void deleteContactTest() {
+        Contact contact = new Contact();
+        contact.setId(1);
+        contact.setName("One");
+        contact.setSurname1("Surname1One");
+        contact.setSurname2("Surname2One");
+        contact.setPhone("666555444");
+        contact.setEmail("contact-one@gmail.com");
 
-		ContactDTO editContactDTO = ContactMapper.INSTANCE.toDTO(contact);
+        ContactDTO editContactDTO = ContactMapper.INSTANCE.toDTO(contact);
 
-		int deleteContactId = contactService.deleteContact(editContactDTO);
+        Integer deleteContactId = this.contactService.deleteContact(editContactDTO);
 
-		assertNotNull(deleteContactId);
-	}
+        assertNotNull(deleteContactId);
+    }
 }
