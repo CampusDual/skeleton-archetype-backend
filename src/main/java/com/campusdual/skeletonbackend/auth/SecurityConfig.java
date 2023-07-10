b0ef095ca1b14a066f39658fc3bb57471ac60bed
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-07-10 11:31:40 +0200

    Add: Add CORS configuration for Angular deployment. Change: Remove unnecessary methods.

--- src/main/java/com/campusdual/skeletonbackend/auth/SecurityConfig.java
+++ src/main/java/com/campusdual/skeletonbackend/auth/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.campusdual.skeletonbackend.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,11 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
