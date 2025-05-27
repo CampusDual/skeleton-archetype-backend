f3951080d6d9174ca5cb5010bf8cbf5197709826
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2025-05-27 16:54:51 +0200

    Add auth via JWT

--- src/main/java/com/campusdual/skeletonbackend/model/User.java
+++ src/main/java/com/campusdual/skeletonbackend/model/User.java
@@ -1,8 +1,22 @@
 package com.campusdual.skeletonbackend.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "USERS")
